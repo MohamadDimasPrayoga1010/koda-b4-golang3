ddaefd94f10899971389d703e947639492d8dd04
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-10-21 22:11:04 +0700

    feat: implement main program for user name search

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"main.go/search"
+)
+
+
+func main(){
+	users := []string {"Yoga", "Dimas", "Itsna", "Sidik", "Fiki", "Ari", "Anggi", "Federus"}
+
+	var keyword string
+	fmt.Println("Masukan Nama yang ingin dicari")
+	fmt.Scan(&keyword)
+
+	userName := search.SearchUser(users, keyword)
+
+	if len(userName) > 0{
+		fmt.Println("Nama Ditemukan : ", userName)
+	} else{
+		fmt.Println("Nama Tidak Ditemukan")
+	}
+}
