ecefb2505027a7927dc5b3f2d9e0395fd7e9de82
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-10-22 11:58:29 +0700

    feat: make keywords into pointers

--- search/search.go
+++ search/search.go
@@ -2,14 +2,15 @@ package search
 
 import "strings"
 
-func SearchUser(users[]string, keyword string) []string{
+func SearchUser(users[]string, keyword *string) []string{
 	var result []string
-	keyword = strings.ToLower(keyword)
+	*keyword = strings.ToLower(*keyword)
 
 	for _, name := range users {
-		if strings.ToLower(name) == keyword { 
+		if strings.ToLower(name) == *keyword { 
 			result = append(result, name)
 		}
 	}
 	return result
 }
+
