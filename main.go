adcd82d5940cc4949f793cc8b62b3d9e1e056672
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-10-22 09:33:43 +0700

    feat: improve output

--- main.go
+++ main.go
@@ -2,12 +2,12 @@ package main
 
 import (
 	"fmt"
+
 	"main.go/search"
 )
 
-
-func main(){
-	users := []string {"Yoga", "Dimas", "Itsna", "Sidik", "Fiki", "Ari", "Anggi", "Federus"}
+func main() {
+	users := []string{"Yoga", "Dimas", "Itsna", "Sidik", "Fiki", "Ari", "Anggi", "Federus"}
 
 	var keyword string
 	fmt.Println("Masukan Nama yang ingin dicari")
@@ -15,9 +15,11 @@ func main(){
 
 	userName := search.SearchUser(users, keyword)
 
-	if len(userName) > 0{
-		fmt.Println("Nama Ditemukan : ", userName)
-	} else{
-		fmt.Println("Nama Tidak Ditemukan")
-	}
+	fmt.Println("Hasil Pencarian:", userName)
+
+	// if len(userName) > 0{
+	// 	fmt.Println("Nama Ditemukan : ", userName)
+	// } else{
+	// 	fmt.Println("Nama Tidak Ditemukan")
+	// }
 }
