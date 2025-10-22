e2412235e7e1062acaf549776c08d34e40c99f13
Mohamad Dimas Prayoga <mohamaddimasprayoga89@gmail.com>
2025-10-22 11:57:31 +0700

    feat: added defer, recover and panic for validation

--- main.go
+++ main.go
@@ -2,20 +2,35 @@ package main
 
 import (
 	"fmt"
-
+	"os"
 	"main.go/search"
 )
 
 func main() {
+	// defer fmt.Println("Program Telah Di Jalankan")
+	defer func(){
+		if r := recover(); r != nil{
+			fmt.Println("Terjadi Kesalahan", r)
+			os.Exit(1)
+		} else{
+			fmt.Println("Program Berjalan")
+		}
+	}()
 	users := []string{"Yoga", "Dimas", "Itsna", "Sidik", "Fiki", "Ari", "Anggi", "Federus"}
 
 	var keyword string
 	fmt.Println("Masukan Nama yang ingin dicari")
 	fmt.Scan(&keyword)
+	
+	
+	userName := search.SearchUser(users, &keyword)
 
-	userName := search.SearchUser(users, keyword)
-
+	if len(userName) == 0 {
+		fmt.Println("Hasil Pencarian:", userName)
+		panic("Nama Tidak Ditemukan")
+	}
 	fmt.Println("Hasil Pencarian:", userName)
+
 
 	// if len(userName) > 0{
 	// 	fmt.Println("Nama Ditemukan : ", userName)
